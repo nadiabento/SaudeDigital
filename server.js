cf86f428d637e38d9927b8d4f2285a28d3303ce6
nadiabento <nadia-bento@hotmail.com>
2026-04-21 10:33:30 +0100

    ligaçao node

--- server.js
+++ server.js
@@ -1,24 +1,43 @@
-const express = require('express');
-const db = require('./src/config/db'); 
-require('dotenv').config();
+const express = require("express");
+const db = require("./src/config/db");
+const exameRoutes = require("./src/routes/exameRoutes"); // Importar as novas rotas
+require("dotenv").config();
 
 const app = express();
-
-// FASE I: Servir as páginas HTML/CSS estáticas 
-app.use(express.static('public'));
-
 const PORT = process.env.PORT || 3000;
 
-// FASE I: Testar se a Base de Dados está online no servidor da ESTGA
+// Permite que o Express entenda dados enviados em formato JSON (essencial para API)
+app.use(express.json());
+
+// Serve os ficheiros estáticos (HTML, CSS, JS do frontend)
+app.use(express.static("public"));
+
+// ROTAS DA AP
+
+// Regista as rotas de exames com o prefixo /api/exames
+app.use("/api/exames", exameRoutes);
+
+//INICIALIZAÇÃO E TESTE DA DB ---
+
 db.getConnection()
-    .then((connection) => {
-        console.log('SUCESSO: A Base de Dados da ESTGA (Fase I) está ligada!');
-        connection.release(); 
-        
-        app.listen(PORT, () => {
-            console.log(`Páginas HTML da Fase I a correr em http://localhost:${PORT}`);
-        });
-    })
-    .catch((erro) => {
-        console.error('ERRO: Não foi possível ligar à Base de Dados.', erro.message);
+  .then((connection) => {
+    console.log("SUCESSO: Ligação à Base de Dados da ESTGA estabelecida!");
+    connection.release();
+
+    app.listen(PORT, () => {
+      console.log(`Servidor a correr em http://localhost:${PORT}`);
+      console.log(`Frontend: http://localhost:${PORT}`);
+      console.log(
+        `API Categorias: http://localhost:${PORT}/api/exames/categorias`,
+      );
     });
+  })
+  .catch((erro) => {
+    console.error(
+      "ERRO: Falha crítica na ligação à Base de Dados:",
+      erro.message,
+    );
+  });
+
+console.log("Conteúdo de exameRoutes:", exameRoutes);
+app.use("/api/exames", exameRoutes);
